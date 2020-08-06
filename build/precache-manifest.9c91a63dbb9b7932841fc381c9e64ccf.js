a93dfe9e9f1771fc1dcad7962c4ecfe4cce34ef2
PSantosWave <s.paula@ua.pt>
2020-08-06 15:00:58 +0100

    build

---
+++ build/precache-manifest.9c91a63dbb9b7932841fc381c9e64ccf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "303410ee8b4f9add7aa3dca69066ce07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ac2595e771c99403ab5",
+    "url": "/static/css/main.6e0ce5a7.chunk.css"
+  },
+  {
+    "revision": "0a08fc153600d2079bef",
+    "url": "/static/js/2.5fd5be95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5fd5be95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ac2595e771c99403ab5",
+    "url": "/static/js/main.4f80a18a.chunk.js"
+  },
+  {
+    "revision": "b9afea1be57dcef7bc87",
+    "url": "/static/js/runtime-main.37dd25c1.js"
+  },
+  {
+    "revision": "a4b068ee8a8bdb4d976648992bb1db90",
+    "url": "/static/media/Manrope-ExtraLight.a4b068ee.ttf"
+  },
+  {
+    "revision": "1b164db777679f839df437856cfe762b",
+    "url": "/static/media/logo.1b164db7.png"
+  }
+]);
